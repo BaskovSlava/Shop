d7f15eb04e608b8c50d5d959afb53b7110a35368
Stanislav Dashkevitch <stasdaskevic6@gmail.com>
2024-03-07 16:37:28 +0300

    logo

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import NotFoundedBlock from './components/NotFoundedBlock';
 
 import Home from './pages/Home';
 import Header from './components/Header';
+import SearchEngine from './components/SearchEngine';
 
 import './sass/app.scss';
 
 function App() {
   return (
     <div>
-      <Header></Header>
+      <Header/>
+      <SearchEngine/>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='*' element={<NotFoundedBlock/>}/>
